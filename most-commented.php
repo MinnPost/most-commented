6ebcff1900b0055ede122bf9bbb13e638525be3f
MtDewVirus <MtDewVirus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-02-23 22:33:52 +0000

    Added option for Last X days (props DJ Chuang)
    git-svn-id: https://plugins.svn.wordpress.org/most-commented/trunk@8051 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- most-commented.php
+++ most-commented.php
@@ -2,17 +2,21 @@
 /*
 Plugin Name: Most Commented
 Plugin URI: http://mtdewvirus.com/code/wordpress-plugins/
-Description: Retrieves a list of the posts with the most comments.
-Version: 1.02
+Description: Retrieves a list of the posts with the most comments. Modified for Last X days -- by DJ Chuang www.djchuang.com 
+Version: 1.4
 Author: Nick Momrik
 Author URI: http://mtdewvirus.com/
 */
 
-function mdv_most_commented($no_posts = 5, $before = '<li>', $after = '</li>', $show_pass_post = false) {
+function mdv_most_commented($no_posts = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $duration='') {
     global $wpdb;
 	$request = "SELECT ID, post_title, COUNT($wpdb->comments.comment_post_ID) AS 'comment_count' FROM $wpdb->posts, $wpdb->comments";
 	$request .= " WHERE comment_approved = '1' AND $wpdb->posts.ID=$wpdb->comments.comment_post_ID AND post_status = 'publish'";
 	if(!$show_pass_post) $request .= " AND post_password =''";
+
+        if($duration !="") { $request .= " AND DATE_SUB(CURDATE(),INTERVAL ".$duration." DAY) < post_date ";
+}
+
 	$request .= " GROUP BY $wpdb->comments.comment_post_ID ORDER BY comment_count DESC LIMIT $no_posts";
     $posts = $wpdb->get_results($request);
     $output = '';
